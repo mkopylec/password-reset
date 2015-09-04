23e871af3d1825f3bad9c50aa1c73a5c6b7f188d
Mariusz Kopylec <mariusz.kopylec@allegro.pl>
2015-09-04 15:23:34 +0200

    part 1

---
+++ src/main/java/com/github/mkopylec/passwordreset/api/Endpoints.java
@@ -0,0 +1,29 @@
+package com.github.mkopylec.passwordreset.api;
+
+public final class Endpoints {
+
+    /**
+     * Response:
+     * <pre>
+     * <code>{
+     *     "id": user_id,
+     *     "passwordResetMethod": "RESET_METHOD"
+     * }
+     * </code>
+     * </pre>
+     */
+    public static final String USER_RESET_METHOD = "users/{loginOrEmail}";
+
+    /**
+     * Response:
+     * <pre>
+     * <code>{
+     *     "id": user_id,
+     *     "passwordResetMethod": "RESET_METHOD"
+     * }
+     * </code>
+     * </pre>
+     */
+    public static final String USER_RESET_METHOD = "users/{loginOrEmail}";
+
+}
