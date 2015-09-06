5b7e03c4b31b825d335741e90298801158530790
Mariusz Kopylec <mariusz.kopylec@allegro.pl>
2015-09-06 21:35:38 +0200

    i am tired

--- src/main/java/com/github/mkopylec/passwordreset/application/EmailService.java
+++ src/main/java/com/github/mkopylec/passwordreset/application/EmailService.java
@@ -1,12 +1,26 @@
 package com.github.mkopylec.passwordreset.application;
 
 import com.github.mkopylec.passwordreset.api.dto.ResetData;
+import com.github.mkopylec.passwordreset.domain.user.User;
+import com.github.mkopylec.passwordreset.domain.user.UserRepository;
 import org.springframework.stereotype.Service;
+
+import static com.github.mkopylec.passwordreset.application.check.Preconditions.notFoundIfNull;
 
 @Service
 class EmailService {
 
+    private final UserRepository userRepository;
+    private final EmailSender emailSender;
+
+    public EmailService(UserRepository userRepository, EmailSender emailSender) {
+        this.userRepository = userRepository;
+        this.emailSender = emailSender;
+    }
+
     public void sendPasswordResetEmail(long id, ResetData resetData) {
-        throw new UnsupportedOperationException();
+        User user = userRepository.findById(id);
+        notFoundIfNull(user, "User with id: '" + id + "' does not exist");
+        emailSender.sendPasswordResetEmail(user, resetData.getResetUrl());
     }
 }
