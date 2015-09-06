a5b5005f54e7d2e71992b1cba446c76a7e82995d
Mariusz Kopylec <mariusz.kopylec@allegro.pl>
2015-09-06 22:13:51 +0200

    miaden added to reset data

--- src/main/java/com/github/mkopylec/passwordreset/api/dto/ResetData.java
+++ src/main/java/com/github/mkopylec/passwordreset/api/dto/ResetData.java
@@ -3,6 +3,7 @@ package com.github.mkopylec.passwordreset.api.dto;
 public class ResetData {
 
     private ResetMethod resetMethod;
+    private String maidenName;
     private String resetUrl;
 
     public ResetMethod getResetMethod() {
@@ -11,6 +12,14 @@ public class ResetData {
 
     public void setResetMethod(ResetMethod resetMethod) {
         this.resetMethod = resetMethod;
+    }
+
+    public String getMaidenName() {
+        return maidenName;
+    }
+
+    public void setMaidenName(String maidenName) {
+        this.maidenName = maidenName;
     }
 
     public String getResetUrl() {
