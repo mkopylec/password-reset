ebedb459784706549984c1db7076f9b8ceadfb84
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2015-09-30 19:20:18 +0200

    fail fast for password reset email

--- src/main/java/com/github/mkopylec/passwordreset/domain/notification/PasswordResetEmail.java
+++ src/main/java/com/github/mkopylec/passwordreset/domain/notification/PasswordResetEmail.java
@@ -1,5 +1,8 @@
 package com.github.mkopylec.passwordreset.domain.notification;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 public class PasswordResetEmail {
 
     private final String recipientEmail;
@@ -7,6 +10,9 @@ public class PasswordResetEmail {
     private final String message;
 
     PasswordResetEmail(String recipientEmail, String subject, String message) {
+        checkArgument(isNotBlank(recipientEmail), "Recipient e-mail address cannot be empty");
+        checkArgument(isNotBlank(subject), "E-mail subject cannot be empty");
+        checkArgument(isNotBlank(message), "E-mail message cannot be empty");
         this.recipientEmail = recipientEmail;
         this.subject = subject;
         this.message = message;
